c2faea95998e00a25b55a1b8f55fdee57f225478
RTQL <v.a.kmnski@gmail.com>
2022-12-12 00:24:08 +0400

    refactor using new knowledges

--- node-js/133-homework/math.js
+++ node-js/133-homework/math.js
@@ -1,20 +1,24 @@
 /*
  * Домашнее задание: реализуйте функции sum, mul и avg
  */
+const sum = function() {
+    /* возвращает сумму аргументов */
+       const args = Array.from(arguments)
+       return args.reduce((acc, val) => acc + val)
+       }
+const mul = function() {
+        /* возвращает произведение аргументов */
+       const args = Array.from(arguments)
+       return args.reduce((acc, val) => acc * val)
+       }
+const avg = function() {
+        /* возвращает среднее арифметическое аргументов */
+       const args = Array.from(arguments)
+       return sum(...args) / args.length
+       }
+
 module.exports = {
-    sum: function(a=0, b=0, c=0, d=0) {
-     /* возвращает сумму аргументов */
-        return a + b + c + d
-        },
-    mul: function(a=1, b=1, c=1, d=1) {
-         /* возвращает произведение аргументов */
-        return a * b * c * d
-        },
-    avg: function(a=0, b=0, c=0, d=0) {
-         /* возвращает среднее арифметическое аргументов */
-        let quantity = 2
-        if (c !== 0) {quantity = 3}
-        if (d !== 0) {quantity = 4}
-        return (a + b + c + d) / quantity
-        },
+    sum,
+    mul,
+    avg
 }
